d203106f10fe98b7455f2976bf2d51aa1933b635
Vangelis Mazis <mazhs1992@hotmail.com>
2025-09-20 15:55:46 +0300

    feat: initial WinForms solution with .gitignore

---
+++ src/TouristGuide.WinForms/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TouristGuide.WinForms
+{
+    public partial class MainForm : Form
+    {
+        private Label lblWelcome;
+        private Button btnBeaches;
+        private Button btnAttractions;
+        private Button btnRestaurants;
+
+    
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+        
---
+++ src/TouristGuide.WinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TouristGuide.WinForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TouristGuide.WinForms")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("371c6368-d839-4ede-95ab-9e722c9c6513")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
