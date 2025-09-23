0620d96a6c883b11e658ea394afb669f0fb84dc3
Xristos04 <xristosptol5@yahoo.gr>
2025-09-23 22:49:11 +0300

    Completed UI and cs (with database)

--- src/TouristGuide.WinForms/LoginForm.Designer.cs
+++ src/TouristGuide.WinForms/LoginForm.Designer.cs
@@ -28,21 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.login_panel = new System.Windows.Forms.Panel();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_visitor = new System.Windows.Forms.Button();
+            this.button_user = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.login_panel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // login_panel
+            // 
+            this.login_panel.BackgroundImage = global::TouristGuide.WinForms.Properties.Resources.login_icon;
+            this.login_panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.login_panel.Controls.Add(this.textBox_password);
+            this.login_panel.Controls.Add(this.button_visitor);
+            this.login_panel.Controls.Add(this.button_user);
+            this.login_panel.Controls.Add(this.label3);
+            this.login_panel.Controls.Add(this.textBox_username);
+            this.login_panel.Controls.Add(this.label2);
+            this.login_panel.Controls.Add(this.label1);
+            this.login_panel.Location = new System.Drawing.Point(154, 12);
+            this.login_panel.Name = "login_panel";
+            this.login_panel.Size = new System.Drawing.Size(481, 426);
+            this.login_panel.TabIndex = 0;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox_password.Location = new System.Drawing.Point(172, 316);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(149, 22);
+            this.textBox_password.TabIndex = 7;
+            // 
+            // button_visitor
+            // 
+            this.button_visitor.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button_visitor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button_visitor.ForeColor = System.Drawing.SystemColors.Window;
+            this.button_visitor.Location = new System.Drawing.Point(318, 373);
+            this.button_visitor.Name = "button_visitor";
+            this.button_visitor.Size = new System.Drawing.Size(141, 40);
+            this.button_visitor.TabIndex = 6;
+            this.button_visitor.Text = "Log in (as Visitor)";
+            this.button_visitor.UseVisualStyleBackColor = false;
+            this.button_visitor.Click += new System.EventHandler(this.button_visitor_Click);
+            // 
+            // button_user
+            // 
+            this.button_user.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button_user.ForeColor = System.Drawing.SystemColors.Window;
+            this.button_user.Location = new System.Drawing.Point(20, 373);
+            this.button_user.Name = "button_user";
+            this.button_user.Size = new System.Drawing.Size(141, 40);
+            this.button_user.TabIndex = 1;
+            this.button_user.Text = "Log in (as User)";
+            this.button_user.UseVisualStyleBackColor = false;
+            this.button_user.Click += new System.EventHandler(this.button_user_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(180, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password :";
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox_username.Location = new System.Drawing.Point(172, 258);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(149, 22);
+            this.textBox_username.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(188, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(151, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Page";
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.login_panel);
             this.Name = "LoginForm";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.login_panel.ResumeLayout(false);
+            this.login_panel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel login_panel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_visitor;
+        private System.Windows.Forms.Button button_user;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- src/TouristGuide.WinForms/LoginForm.cs
+++ src/TouristGuide.WinForms/LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,48 @@ namespace TouristGuide.WinForms
         private void LoginForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button_user_Click(object sender, EventArgs e)
+        {
+            string username = textBox_username.Text.Trim();
+            string password = textBox_password.Text.Trim();
+            string connectionString = @"Data Source=Tourist_Guide.db;Version=3;";
+
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT COUNT(1) FROM Users WHERE Username=@username AND Password=@password";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count == 1)
+                    {
+                        MessageBox.Show("Login successful!");
+                        // Άνοιξε το main form
+                        MainForm main = new MainForm();
+                        main.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password.");
+                    }
+                }
+            }
+        }
+
+        private void button_visitor_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Login as Visitor!");
+            MainForm main = new MainForm();
+            main.Show();
+            this.Hide();
         }
     }
 }
