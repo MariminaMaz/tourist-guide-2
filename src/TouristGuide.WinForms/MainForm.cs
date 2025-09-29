93bb02bbff309d400fd4ef6dca70dd5d6ebb0190
MariminaMaz <marimina96@gmail.com>
2025-09-29 16:10:44 +0300

    user/visitor history

--- src/TouristGuide.WinForms/MainForm.cs
+++ src/TouristGuide.WinForms/MainForm.cs
@@ -37,6 +37,47 @@ namespace TouristGuide.WinForms
         {
 
         }
+
+        private void button_beaches_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_attractions_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_History_Click(object sender, EventArgs e)
+        {
+            if (Session.IsVisitor)
+            {
+                MessageBox.Show("Οι επισκέπτες δεν έχουν ιστορικό!");
+            }
+            else
+            {
+                MessageBox.Show("Τελευταία παραλία που είδατε: Ναυάγιο");
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (Session.IsVisitor)
+            {
+                MessageBox.Show("Welcome Visitor! Limited functionality.");
+
+                // Κρύβει το κουμπί Ιστορικό για Visitor
+                button_History.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show($"Welcome {Session.Username}!");
+
+                // Ο User βλέπει το κουμπί Ιστορικό
+                button_History.Visible = true;
+            }
+        
+        }
     }
 }
 
